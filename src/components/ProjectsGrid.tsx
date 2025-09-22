99862bd8e7612979ba90d1684c89bd9b2c9c7f9d
the13thgeek <aldreich.aguilar@yahoo.com>
2025-09-21 22:10:10 -0400

    Converting Grid to React

---
+++ src/components/ProjectsGrid.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import projects from "../data/projects.json";
+import "./ProjectsGrid.scss";
+
+interface ProjectStackItem {
+  name: string;
+  icon: string;
+}
+
+interface ProjectItem {
+  id: string;
+  title: string;
+  description: string;
+  type: string;
+  url: string | null;
+  gridClass: string;
+  stack: ProjectStackItem[];
+  notes?: string;
+}
+
+/* type GridClass =
+  | "featured-item"
+  | "wide-item"
+  | "tall-item"
+  | "regular-1"
+  | "regular-2"
+  | "small-item"; */
+
+export default function ProjectsGrid() {
+  const [selected, setSelected] = useState<ProjectItem | null>(null);
+
+  return (
+    <>
+      <ul className="projects">
+        {projects.map((project: ProjectItem) => (
+          <li
+            key={project.id}
+            className={`project-item ${project.type} ${project.gridClass} ${project.id}`}
+            onClick={() => setSelected(project)}
+          >
+            <h3>{project.title}</h3>
+          </li>
+        ))}
+      </ul>
+
+      {/* <Modal project={selected} onClose={() => setSelected(null)} /> */}
+    </>
+  );
+}
