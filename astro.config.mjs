a8584a0d7846e58f602711b74f5908f9c6dcc03b
the13thgeek <aldreich.aguilar@yahoo.com>
2025-10-21 21:42:07 -0400

    Blog integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://the13thgeek.com',
-  integrations: [react()]
+  integrations: [react()],
+  base: '/'
 });
